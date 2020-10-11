b546005d01592f9890ee866ab885948bdcbb1c95
Arturo Paz <arturopaz.1910@gmail.com>
2020-10-11 16:22:06 -0500

    Se importan estilos en el contenedor de rutas de la aplicación.

--- src/containers/routes/App.js
+++ src/containers/routes/App.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Switch, Route, Redirect } from 'react-router-dom'
 import Todo from '../../components/todo/Todo'
-
+import '../../components/components.css'
 function App() {
   return (
     <Switch>
