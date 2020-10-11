a94c4f2cba2c98307a3ce8cceedb6759e22f836c
Arturo Paz <arturopaz.1910@gmail.com>
2020-10-11 16:21:13 -0500

    Se agrega helper de lista de tareas.

---
+++ src/helpers/tasks.js
@@ -0,0 +1,82 @@
+export const tasks = [
+    {
+        id: 1,
+        description: 'task 1',
+        duration: 70,
+        left: 10,
+        isFinished: true,
+        started: false
+    },
+    {
+        id: 2,
+        description: 'task 2',
+        duration: 40,
+        left: 20,
+        isFinished: true,
+        started: false
+    },
+    {
+        id: 3,
+        description: 'task 3',
+        duration: 30,
+        left: 10,
+        isFinished: true,
+        started: false
+    },
+    {
+        id: 4,
+        description: 'task 4',
+        duration: 50,
+        left: 5,
+        isFinished: true,
+        started: false
+    },
+    {
+        id: 5,
+        description: 'task 5',
+        duration: 20,
+        left: 2,
+        isFinished: true,
+        started: false
+    },
+    {
+        id: 6,
+        description: 'task 6',
+        duration: 70,
+        left: 10,
+        isFinished: true,
+        started: false
+    },
+    {
+        id: 7,
+        description: 'task 7',
+        duration: 30,
+        left: 30,
+        isFinished: false,
+        started: false
+    },
+    {
+        id: 8,
+        description: 'task 8',
+        duration: 70,
+        left: 70,
+        isFinished: false,
+        started: false
+    },
+    {
+        id: 9,
+        description: 'task 9',
+        duration: 40,
+        left: 40,
+        isFinished: false,
+        started: false
+    },
+    {
+        id: 10,
+        description: 'task 10',
+        duration: 90,
+        left: 90,
+        isFinished: false,
+        started: false
+    }
+]
